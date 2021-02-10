6029daa4af6f08d8e5bde8260b2e2050ab29ccc4
antoinelaly <antoine.laly@gmail.com>
2021-02-10 15:46:17 +0100

    first

--- modal.js
+++ modal.js
@@ -27,26 +27,16 @@ function closeForm() {
   modalbg.style.display = "none";
 }
 
+const inputFirst = document.querySelector('input[name=first]');
+inputFirst.addEventListener('keyup', function(e) {
+ var resultFirst = document.getElementById("first-validation");
 
-const formFields = document.querySelectorAll('input');
-const patterns = {
-  first: /[a-zA-Z]{2,64}/,
-  last: /[a-zA-Z]{2,64}/,
-  email: /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/
-};
-
-// validation function
-function validate(field, regex) {
-  if(regex.test(field.value)) {
-    field.className = 'valid'
-  } else {
-    field.className = 'invalid'
-  }
-};
-
-formFields.forEach(formField => {
-  formField.addEventListener('keyup', (e) => {
-    //console.log(e.target.attributes.name.value)
-    validate(e.target, patterns[e.target.attributes.name.value]);
-  })
+   var regFirst = /[a-zA-Z]{2,64}/;
+    var value = e.target.value;
+    if (value.match(regFirst)) {
+      resultFirst.innerHTML = "";
+    } else {
+      resultFirst.innerHTML = "Veuillez entrer 2 caractères ou plus pour le champ du nom.";
+    }
 });
+
