8ef5810b6adbb1b423d7e9d6ea9631d1b8500d35
antoinelaly <antoine.laly@gmail.com>
2021-02-10 12:01:24 +0100

    first

--- modal.js
+++ modal.js
@@ -11,13 +11,41 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeBtn = document.querySelectorAll(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+// close modal event
+closeBtn.forEach((close) => close.addEventListener("click", closeForm));
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
+function closeForm() {
+  modalbg.style.display = "none";
+}
 
+const inputFirst = document.querySelector('input[name=first]');
+inputFirst.addEventListener('keyup', function(e) {
+ var resultFirst = document.getElementById("first-validation");
+
+   var regFirst = /[a-zA-Z]{2,64}/;
+    var value = e.target.value;
+    if (value.match(regFirst)) {
+      resultFirst.innerHTML = "Code valide";
+    } else {
+      resultFirst.innerHTML = "Veuillez entrer 2 caractères ou plus pour le champ du nom.";
+    }
+});
+
+
+/*const formFields = document.querySelectorAll('input');
+const patterns = {
+  first: /[a-zA-Z]{2,64}/,
+  last: /[a-zA-Z]{2,64}/,
+  password: /^[\w@-]{8,20}$/,
+  slug: /^[a-z\d-]{8,20}$/,
+  email: /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/
+}*/
